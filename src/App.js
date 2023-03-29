330cc17f581452a2ace5223f3fea2cf5fe3e9252
carrotforrabbit <parasyuta.anna@gmail.com>
2023-03-30 00:37:40 +0100

    add another Card rendering

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import "./App.scss";
 import Header from "./components/Header/Header.jsx";
 import React from "react";
 import List from "./components/List/List";
-import CardSlider from "./components/CardSlider/CardSlider";
+import Card from "./components/Card/Card";
 import Error from "./components/Error/Error";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <React.Fragment>
-      <Router>
+    <Router>
+      <React.Fragment>
         <Header />
         <main>
           <Routes>
             <Route path="/home" element={<List />}></Route>
-            <Route path="/cards" element={<CardSlider />}></Route>
+            <Route path="/cards" element={<Card />}></Route>
             {/* <Routes>
             <Route path="/liked" element={<Like />}></Route>
           </Routes> */}
@@ -22,8 +22,8 @@ function App() {
             <Route path="/" element={<List />}></Route>
           </Routes>
         </main>
-      </Router>
-    </React.Fragment>
+      </React.Fragment>
+    </Router>
   );
 }
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,23 +1,52 @@
 import React, { useState } from "react";
-import "../Card/_card.scss";
 import Like from "./../Like/Like.jsx";
+import "../CardSlider/CardSlider.scss";
+import "../Card/_card.scss";
+import BtnPrev from "./../BtnPrev/BtnPrev.jsx";
+import BtnNext from "./../BtnNext/BtnNext.jsx";
+import words from "../../utils/words";
 
-const Card = ({ index, word, transcription, translation }) => {
+const Card = () => {
   const [pressed, setPressed] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [clickedPrev, setPrevClick] = useState(false);
+  const [clickedNext, setNextClick] = useState(false);
+  const wordid = words[index];
+
+  const goToPrevCard = () => {
+    if (index - 1 < 0) {
+      setIndex(words.length - 1);
+    } else {
+      setIndex(index - 1);
+      setPrevClick(!clickedPrev);
+    }
+  };
+  const goToNextCard = () => {
+    if (index + 1 >= words.length) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+      setNextClick(!clickedNext);
+    }
+  };
+
   const showWord = () => {
     setPressed(!pressed);
   };
 
   return (
     <div className="card-container">
+      <BtnPrev onClick={goToPrevCard} />
       {!pressed ? (
         <>
           <div className="card">
             <Like />
-            <p className="card__title visually-hidden ">{index}</p>
-            <p className="card__title">{word}</p>
-            <p className="card__text">{transcription}</p>
-            <p className="card__subtitle visually-hidden">{translation}</p>
+            <p className="card__title visually-hidden ">{wordid.key}</p>
+            <p className="card__title">{wordid.word}</p>
+            <p className="card__text">{wordid.transcription}</p>
+            <p className="card__subtitle visually-hidden">
+              {wordid.translation}
+            </p>
             <button className="card__btn" onClick={showWord}>
               Show tranlation
             </button>
@@ -27,14 +56,15 @@ const Card = ({ index, word, transcription, translation }) => {
         <>
           <div className="card">
             <Like />
-            <p className="card__title visually-hidden ">{index}</p>
-            <p className="card__subtitle">{translation}</p>
+            <p className="card__title visually-hidden ">{wordid.key}</p>
+            <p className="card__subtitle">{wordid.translation}</p>
             <button className="card__btn" onClick={showWord}>
               Show word
             </button>
           </div>
         </>
       )}
+      <BtnNext onClick={goToNextCard} />
     </div>
   );
 };
